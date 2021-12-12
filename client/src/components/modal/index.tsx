5489797ed8a98e6e6c5b10a215b8053814172515
Will Hartman <me@willhartman.co.uk>
2021-12-12 03:16:15 +0000

    feat(socket-concept): Continue PoC
    Co-authored-by: Ashley Brown <proevilz@users.noreply.github.com>

---
+++ client/src/components/modal/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./modal.module.css";
+
+interface IProps {
+  message: string;
+  user: string;
+}
+const Modal = () => {
+  const handleClick = () => {
+
+  }
+  return (
+    <div className={styles.backdrop}>
+      <div className={styles.modal}>
+        <h1>Who are you?</h1>
+        <input type="text" placeholder="Username" onChange={handleChange}/>
+        <button onClick={handleClick}>Enter</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
