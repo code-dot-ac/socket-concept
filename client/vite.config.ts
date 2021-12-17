c483541fd20c7c4e552160588993b09ce311f5ed
Will <me@willhartman.co.uk>
2021-12-17 03:32:35 +0000

    feat(socket-concept): GH Codespaces / VS DevContainer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const codespaceName = 'willhartman-code-dot-ac-socket-concept-7v55w5qgcrx9v'; //process.env['CODESPACE_NAME'];
+const hmrPort = 3000;
+
+const hmrRemoteHost = codespaceName ? `${codespaceName}-${hmrPort}.githubpreview.dev` : 'localhost';
+const hmrRemotePort = codespaceName ? 443 : hmrPort;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      host: hmrRemoteHost,
+      port: hmrPort,
+      clientPort: hmrRemotePort,
+    },
+  },
 });
